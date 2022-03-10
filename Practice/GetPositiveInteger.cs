2a39e6c25bb52bc0edbb3ce79444d79fe9dd17f3
Allison Anguiano <aanguiano@dev-10.com>
2022-03-10 08:47:42 -0600

    Made class for method GetPositiveInteger

---
+++ Practice/GetPositiveInteger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    class GetPositiveInteger
+    {
+        public static int GetPositiveInterger(string prompt)
+        {
+            bool validPosInt = false;
+            int value;
+
+            do
+            {
+                Console.Write(prompt);
+                if (!int.TryParse(Console.ReadLine(), out value))
+                {
+                    Console.WriteLine("That wasn't a number");
+                }
+                else
+                {
+                    if (value > 0)
+                    {
+                        validPosInt = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Value must be positive");
+                    }
+                }
+            } while (!validPosInt);
+            return value;
+        }
+    }
+}
--- Practice/NotMain.cs
+++ Practice/NotMain.cs
@@ -38,7 +38,7 @@ namespace Practice
             //AllowedToParty
             //IsAllowedWithdrawal
             //RollPassGo
-            int rollNum = GetPositiveInterger("What did you roll? ");
+            int rollNum = GetPositiveInteger.GetPositiveInterger("What did you roll? ");
             Console.WriteLine($"You rolled a {rollNum} ");
             if (rollNum > 7 && rollNum < 12)
             {
@@ -68,33 +68,6 @@ namespace Practice
 
         //    return input;
         //}
-
-        public static int GetPositiveInterger(string prompt)
-        {
-            bool validPosInt = false;
-            int value;
-
-            do
-            {
-                Console.Write(prompt);
-                if (!int.TryParse(Console.ReadLine(), out value))
-                {
-                    Console.WriteLine("That wasn't a number");
-                }
-                else
-                {
-                    if (value > 0)
-                    {
-                        validPosInt = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Value must be positive");
-                    }
-                }
-            } while (!validPosInt);
-            return value;
-        }
 
         //private bool PrinterValid()
         //{
